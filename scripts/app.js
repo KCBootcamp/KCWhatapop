ceff0bcdec9e5b22642dad96c1c120dd286ef4f6
Bhavish Chandnani <bhavish.chandnani@t-systems.com>
2016-06-18 20:09:10 +0200

    Installed Component Router

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-var app =angular.module("whatapop",[]);
+var app =angular.module("whatapop",["ngComponentRouter"]);
 app
     .controller("contro", function ($scope) {
         $scope.busqueda="Buscando";
