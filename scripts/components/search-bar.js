4822999f8a09cc5ab180229d0a85d0de87280b2b
Bhavish Chandnani <bhavish.chandnani@t-systems.com>
2016-06-18 19:46:44 +0200

    Created search bar component

---
+++ scripts/components/search-bar.js
@@ -0,0 +1,10 @@
+angular.module("whatapop")
+    .component("searchBar", {
+        templateUrl:"views/search-bar.html",
+        controller: function () {
+
+        },
+        bindings:{
+
+        }
+    });
--- scripts/directives/search-bar.js
+++ scripts/directives/search-bar.js
@@ -1,3 +1,20 @@
-/**
- * Created by bhavishchandnani on 18/6/16.
- */
+angular.module("whatapop")
+    .directive("bvcSearchBar",  function () {
+        return{
+            restrict:"EA",
+            templateUrl:"views/search-bar.html",
+            scope:{
+                sources: "@",
+                displayInline:"@",
+                onButtonClick:"&"
+            },
+            link: function (scope) {
+                 scope.notifySearch = function () {
+                    scope.onButtonClick({searchData: scope.searchData,
+                        categoryData: scope.categoryData});
+                     console.log("Notificando click", scope.searchData, scope.categoryData);
+                 };
+            }
+        };
+
+    });
