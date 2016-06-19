8da5b96385127244d2b39ffbd1e5a00c05594f2a
Bhavish Chandnani <bhavish.chandnani@t-systems.com>
2016-06-19 17:41:37 +0200

    Modified product details

--- scripts/components/product-detail.js
+++ scripts/components/product-detail.js
@@ -1,9 +1,20 @@
 angular.module("whatapop")
     .component("productDetail", {
         templateUrl:"views/product-detail.html",
-        controller: {
-        },
         bindings:{
+          router:"<"
+        },
+         controller: ["ServiceProducts", function (ServiceProducts) {
 
-        }
+             var self = this;
+
+             self.$routerOnActivate = function(next) {
+                 var productId = next.params.id;
+                 
+                 ServiceProducts.getProductWithId(productId).then(function(res) {
+                     self.product = res.data;
+                 });
+             };
+
+        }]
     });
